3f2a2cd08c55eca478da0243ba0bc254adaff8c0
Russ Miles <russell.miles@opencredo.com>
2011-11-10 16:40:35 +0000

    Updated chapter 2 koans to separate them out for the book

--- programmingspring/chapter-2-factories-solution/src/test/java/com/programmingspring/koans/FactoriesKoan.java
+++ programmingspring/chapter-2-factories-solution/src/test/java/com/programmingspring/koans/FactoriesKoan.java
@@ -6,39 +6,33 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class FactoriesKoan extends TestCase {
 
-    public void testGettingSingletonFromApplicationContext() {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("factories-context.xml");
-        assertNotNull("Please configure Singleton as a bean in the ApplicationContext, with id = 'singletonInstance'", applicationContext.getBean("singletonInstance", Singleton.class));
+	public void testKoan1GettingSingletonFromApplicationContext() {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("gof-singleton-context.xml");
+        assertNotNull("Please configure Singleton as a bean in the ApplicationContext, with id = 'singletonInstance'", 
+        		applicationContext.getBean("singletonInstance", Singleton.class));
     }
 
-    public void testGettingStaticFactoryProduct() {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("factories-context.xml");
+    public void testKoan2GettingStaticFactoryProduct() {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("static-factory-context.xml");
 
         Product product = applicationContext.getBean("staticFactoryProduct", Product.class);
         assertNotNull("Please configure StaticFactory in the ApplicationContext, with id='staticFactoryProduct'", product);
         assertEquals("Please configure StaticFactory in the ApplicationContext so that it produces a Product named 'someProductName'", "someProductName", product.getProductName());
     }
-
-    public void testGettingPojoFactoryProduct() {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("factories-context.xml");
+    
+    public void testKoan3GettingPojoFactoryProduct() {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("pojo-factory-context.xml");
 
         Product product = applicationContext.getBean("pojoFactoryProduct", Product.class);
         assertNotNull("Please configure PojoFactory in the ApplicationContext, with id='pojoFactoryProduct'", product);
         assertEquals("Please configure PojoFactory in the ApplicationContext so that it produces a Product named 'anotherProductName'", "anotherProductName", product.getProductName());
     }
-
-    public void testGettingSpringAwareFactoryProduct() {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("factories-context.xml");
+    
+    public void testKoan4GettingSpringAwareFactoryProduct() {
+    	ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-aware-factory-context.xml");
 
         Product product = applicationContext.getBean("springFactoryBeanProduct", Product.class);
         assertNotNull("Please configure SpringAwareFactory in the ApplicationContext, with id='springFactoryBeanProduct'", product);
         assertEquals("Please configure SpringAwareFactory in the ApplicationContext, with id='springFactoryBeanProduct'", "alwaysTheSame", product.getProductName());
-    }
-
-    public void testGettingServiceWithDependencyProducedByFactory() {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("factories-context.xml");
-
-        ProductDependentService service = applicationContext.getBean("service", ProductDependentService.class);
-        assertEquals("Please configure ProductDependentService with id='service' and inject the SpringAwareFactory's product into its constructor", "alwaysTheSame", service.getProduct().getProductName());
     }
 }
--- programmingspring/chapter-2-factories/src/main/java/com/programmingspring/koans/Singleton.java
+++ programmingspring/chapter-2-factories/src/main/java/com/programmingspring/koans/Singleton.java
@@ -2,11 +2,22 @@ package com.programmingspring.koans;
 
 public class Singleton {
 
-    private static final Singleton INSTANCE = new Singleton();
-
-    private Singleton() {}
+	private String name;
+	
+	private static final String DEFAULT_NAME = "default name";
+    
+	// The single instance
+	private static final Singleton INSTANCE = new Singleton(DEFAULT_NAME);
+    
+    private Singleton(String name) {
+    	this.name = name;
+    }
 
     public static Singleton getInstance() {
         return INSTANCE;
     }
+
+	public String getName() {
+		return name;
+	}
 }
